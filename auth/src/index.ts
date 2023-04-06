1d5d25763fc14003483cd2ce3168eed3e1436b5e
Bryan Fonseca <isaac1805@hotmail.com>
2023-04-06 15:36:25 -0500

    Add MongoDB connection

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
 import {json} from 'body-parser';
+import mongoose from 'mongoose';
+
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signupRouter } from './routes/signup';
@@ -22,6 +24,18 @@ app.all('*', async () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
-});
+const init = async () => {
+    try {
+        // connect using the K8s service
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        console.log('Successfully connected to MongoDB');
+
+        app.listen(3000, () => {
+            console.log('Listening on port 3000');
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+init();
